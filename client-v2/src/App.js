1a88013e900f120ed706be0689d45cc4107042e1
Mark Bruckert <markbruckert1@Gmail.com>
2023-09-16 23:49:45 -0400

    Frontend changes

--- client-v2/src/App.js
+++ client-v2/src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import React, {useState} from 'react';
 import './App.css';
-import {Textarea, Spinner} from '@primer/react'
-import {ThemeProvider} from '@primer/react'
+import {Textarea, Spinner, Button} from '@primer/react'
+import { SyncIcon } from "@primer/octicons-react";
+import {ThemeProvider} from '@primer/react';
 import { Player, BigPlayButton } from 'video-react';
 import 'video-react/dist/video-react.css';
 import { OpenAI } from "langchain/llms/openai";
@@ -120,13 +121,30 @@ function App() {
       <div><Toaster/></div>
     <div className="App" style={{backgroundImage:`url(${process.env.PUBLIC_URL+ "/background.png"})`, width: '100%', height: '135vh', backgroundSize: 'cover'}}>
       <div style={{position: 'absolute', width: '100%'}}>
-        <div style={{width: '100%', height: '70px', backgroundColor: 'white', display: 'flex', gap: '10px', alignItems: 'center', borderBottom: '1px solid #BDBDBD', paddingLeft: '20px', position: 'fixed'}}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6" style={{width: '30px'}}>
-            <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
-            <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z" />
-            <path d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z" />
-          </svg>
-          <h1 style={{color: 'black', fontSize: '25px', fontWeight: '700'}}>GP<span style={{color: '#127C01'}}>Teacher</span></h1>
+          <div style={{width: '100%', height: '70px', backgroundColor: 'white', display: 'flex', gap: '10px', alignItems: 'center', borderBottom: '1px solid #BDBDBD', paddingLeft: '20px', position: 'fixed'}}>
+          <div style={{display: 'flex', justifyContent: 'space-between', width: '97%', alignItems: 'center'}}>
+            <div style={{display: 'flex', gap: '20px'}}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6" style={{width: '30px'}}>
+                <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
+                <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z" />
+                <path d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z" />
+              </svg>
+              <h1 style={{color: 'black', fontSize: '25px', fontWeight: '700'}}>GP<span style={{color: '#127C01'}}>Teacher</span></h1>
+              </div>
+              <Button leadingIcon={SyncIcon} onClick={() => {
+                //reset all states
+                setTopic("");
+                setQuestion("");
+                setAnswerChoices([]);
+                setSelectedOption("");
+                setCorrectAnswer("");
+                setValidatedAnswer({});
+                setLoadedVideo(false);
+                setLoadedAnswer(false);
+                setShowConfetti(false);
+                toast.success('Session has been reset!');
+              }}>Reset Session</Button>
+          </div>
         </div>
         <div style={{display: 'flex'}}>
           <div style={{width: '60%', padding: '20px', marginTop: '70px', display: 'flex', flexDirection: 'column', justifyContent: 'start', alignItems: 'start'}}>
